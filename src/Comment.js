a4f87034567c88ae8c2151b2b1ab0ad0c15a3a0e
Yumacorn <ReachRyanMui@gmail.com>
2021-01-14 17:36:40 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,23 @@
 //your code here
+import React, { Component} from 'react'
+export default class Comment extends Component {
+  
+    render() {
+        return <div className="comment">{this.props.commentText}</div>
+        // return null
+    //   if (this.opacity > 0.2) {
+    //     return (
+    //       <div className="color-box" style={{opacity: this.opacity - 0.1}}>
+    //         {/* your conditional code here! */}
+    //       </div>
+    //     )
+    //   } else {
+    //     return (
+    //       null
+    //     )
+    //   }
+  
+      
+    }
+  
+  }
